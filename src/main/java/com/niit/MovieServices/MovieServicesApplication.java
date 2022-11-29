511f3c8b8d58fab8cd0c040f3063720c112b9347
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-11-29 11:51:36 +0530

    Registering Services With Eureka-Server.

--- src/main/java/com/niit/MovieServices/MovieServicesApplication.java
+++ src/main/java/com/niit/MovieServices/MovieServicesApplication.java
@@ -4,9 +4,11 @@ import com.niit.MovieServices.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieServicesApplication {
 
 	public static void main(String[] args) {
